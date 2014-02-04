e97187009f07149a8f0f5875619d7076bd8340ed
MasonPD <xilinkai@gmail.com>
2014-02-03 16:30:40 -0800

    first commit

---
+++ pixster/ImageCell.h
@@ -0,0 +1,13 @@
+//
+//  ImageCell.h
+//  pixster
+//
+//  Created by Linkai Xi on 2/3/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCell : UICollectionViewCell
+
+@end
---
+++ pixster/SearchViewController.h
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.h
+//  pixster
+//
+//  Created by Timothy Lee on 7/30/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UISearchDisplayDelegate, UISearchBarDelegate>
+
+@property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
+
+@end
